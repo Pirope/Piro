e275999f1b82deaf9367d12dd66c5c188f9b9f27
Pilar Rodriguez <ec1405423@edinburghcollege.ac.uk>
2019-12-15 19:31:40 +0000

    masterPlusMistakes

---
+++ PresentationLayer/DataLayer/Model/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.Model
+{
+    class Client
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string surname { get; set; }
+        public string address1 { get; set; }
+        public string address2 { get; set; }
+        public double locLat { get; set; }
+        public double locLon { get; set; }
+    }
+}
